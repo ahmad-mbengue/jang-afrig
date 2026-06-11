f2934d98598986c5462f3e5a5da69cc9b2f87df2
Ahmad At-Tijani MBENGUE <ahmadattijanimbengue@esp.sn>
2026-06-11 17:04:35 +0000

    Lab 7 : Premier Quiz Fonctionnel (DOM + evenements)

--- data.js
+++ data.js
@@ -3,61 +3,25 @@ const pays = [
 { cca3: "MLI", nom: "Mali", capitale: "Bamako" },
 { cca3: "GHA", nom: "Ghana", capitale: "Accra" },
 { cca3: "KEN", nom: "Kenya", capitale: "Nairobi" },
-{ cca3: "MAR", nom: "Maroc", capitale: "Rabat" }
+{ cca3: "MAR", nom: "Maroc", capitale: "Rabat" },
+{ cca3: "NGA", nom: "Nigéria", capitale: "Abuja" },
+{ cca3: "EGY", nom: "Égypte", capitale: "Le Caire" },
+{ cca3: "CIV", nom: "Côte d'Ivoire", capitale: "Yamoussoukro" },
+{ cca3: "TUN", nom: "Tunisie", capitale: "Tunis" },
+{ cca3: "ETH", nom: "Éthiopie", capitale: "Addis-Abeba" }
 ];
 function choisirAuHasard(tableau) {
 const index = Math.floor(Math.random() * tableau.length);
 return tableau[index];
-console.log(choisirAuHasard(pays));
-console.table(pays);
 }
-
-
-
-const etudiants = [
-    { nom: "Ahmad", note: 15 },
-    { nom: "Awa", note: 12 },
-    { nom: "Moussa", note: 14 },
-    { nom: "Fatou", note: 16 },
-    { nom: "Ibrahima", note: 8 },
-    { nom: "Khadija", note: 18 },
-    { nom: "Omar", note: 13 },
-    { nom: "Mariama", note: 17 },
-    { nom: "Seynabou", note: 9 },
-    { nom: "Cheikh", note: 19 }
-];
-
-const admis = etudiants.filter(e => e.note >= 10);
-
-console.table(admis);
-let somme = 0;
-
-etudiants.forEach(e => {
-    somme += e.note;
-});
-
-let moyenne = somme / etudiants.length;
-
-console.log("Moyenne de la classe :", moyenne);;
-
-
-
-
-
-const capitales = pays.map(p => p.capitale);
-
-console.log(capitales);
-
-
-
-
-const prixFCFA = [1000, 2500, 5000, 10000, 15000];
-
-const prixEuros = prixFCFA.map(prix => prix / 656);
-
-console.log(prixEuros);
-
-
-
-
-
+// Mélange un tableau (algorithme classique d'échange)
+function melanger(tableau) {
+const copie = [...tableau]; // copie du tableau (à prendre tel quel, CM3)
+for (let i = copie.length - 1; i > 0; i--) {
+const j = Math.floor(Math.random() * (i + 1));
+const temp = copie[i];
+copie[i] = copie[j];
+copie[j] = temp;
+}
+return copie;
+}
